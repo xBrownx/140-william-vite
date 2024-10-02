fe4b9284137831dd51815fb1f8ae0fe20d682b0d
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-10-03 09:24:10 +1000

    Update index.jsx

--- src/components/pages/landing/index.jsx
+++ src/components/pages/landing/index.jsx
@@ -7,48 +7,16 @@ import { ArrowContainer, PageContainer } from "./styles.jsx";
 
 const DIST_NAME = "https://d3tbbjv3fiiqdd.cloudfront.net/videos/hls/landing-3sec-500kb.m3u8"
 function Landing({ pageRef, scrollTo }) {
-
-    const playerRef = useRef(null);
-
-    const videoOptions = {
-        autoplay: true,
-        controls: false,
-        loop: true,
-        muted: true,
-        fluid: true,
-        preload: 'auto',
-        sources: [{
-            src: DIST_NAME,
-            type: 'application/x-mpegURL'
-        }],
-    };
-
-    const handlePlayerReady = (player) => {
-        playerRef.current = player;
-
-        // You can handle player events here, for example:
-        player.on('waiting', () => {
-            videojs.log('player is waiting');
-        });
-
-        player.on('dispose', () => {
-            videojs.log('player will dispose');
-        });
-    };
-
     return (
         <Page
             $pageRef={pageRef}
             $fullscreen
         >
             <div>
-                <VideoJS
-                    options={videoOptions}
-                    onReady={handlePlayerReady}
-                />
+                <VideoJS src={DIST_NAME} />
             </div>
         </Page>
-    )
+    );
 }
 
 export default React.memo(Landing);
